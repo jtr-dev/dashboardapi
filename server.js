33359c86b9a9b4dd9b86b1b3738d664edeb0bb1c
Tyler Roberts <dnbxna@gmail.com>
2017-01-02 15:44:17 -0500

    csvtojson

--- server.js
+++ server.js
@@ -1,49 +1,56 @@
 
 var express = require("express");
 var jsonfile = require('jsonfile');
+var csv = require('csvtojson');
 var app = express();
 
-var issues = [];
+function convertCSV(file) {
+    csv()
+        .fromFile(file)
+        .on('json', (jsonObj) => {
+            return jsonObj;
+        })
+        .on('done', (error) => {
+            console.log(error)
+        })
+}
 
+var issues = [];
 var newIssues = [{
-		repo: 'Career Builder',
-		issue: 'Can\'t login',
-		comment: ' Attack dog, run away and pretend to be victim with tail in the air',
-	}, {
-		repo: 'Grocery App',
-		issue: 'Cafe c\'est non noir',
-		comment: 'J\'aime le cafè noir, avec la creme et du sugar. Mais, c\'est non noir a plus. ',
-	}, {
-		repo: 'Grocery App ',
-		issue: 'Ou le pommes ',
-		comment: 'Je ne peux pas trouver les pommes, J\'ai regardé partout ',
-	}, {
-		repo: 'Leo interdum lectus elementum',
-		issue: 'Lorem ipsum dolor sit amet.',
-		comment: '  Ignore the squirrels, you\'ll never catch them ',
-	}, {
-		repo: 'Eu recusandae ',
-		issue: 'Lorem ipsum dolor sit amet.',
-		comment: ' Element convallis risus mauris nam magna wisi, hendrerit dui mauris tincidunt integer in a.',
-	}, {
-		repo: 'Nec porta ',
-		issue: 'Lorem ipsum dolor sit amet.',
-		comment: 'A claw then lick paws clean wash down prawns with a lap of carnation milk then retire to ',
-	}, {
-		repo: 'lacus quam nunc eget',
-		issue: 'Lorem ipsum dolor sit amet.',
-		comment: 'Lay on arms while you\'re using the keyboard eat prawns daintily',
-	}];
+    repo: 'Career Builder',
+    issue: 'Can\'t login',
+    comment: ' Attack dog, run away and pretend to be victim with tail in the air',
+}, {
+    repo: 'Grocery App',
+    issue: 'Cafe c\'est non noir',
+    comment: 'J\'aime le cafè noir, avec la creme et du sugar. Mais, c\'est non noir a plus. ',
+}, {
+    repo: 'Grocery App ',
+    issue: 'Ou le pommes ',
+    comment: 'Je ne peux pas trouver les pommes, J\'ai regardé partout ',
+}, {
+    repo: 'Leo interdum lectus elementum',
+    issue: 'Lorem ipsum dolor sit amet.',
+    comment: '  Ignore the squirrels, you\'ll never catch them ',
+}, {
+    repo: 'Eu recusandae ',
+    issue: 'Lorem ipsum dolor sit amet.',
+    comment: ' Element convallis risus mauris nam magna wisi, hendrerit dui mauris tincidunt integer in a.',
+}, {
+    repo: 'Nec porta ',
+    issue: 'Lorem ipsum dolor sit amet.',
+    comment: 'A claw then lick paws clean wash down prawns with a lap of carnation milk then retire to ',
+}, {
+    repo: 'lacus quam nunc eget',
+    issue: 'Lorem ipsum dolor sit amet.',
+    comment: 'Lay on arms while you\'re using the keyboard eat prawns daintily',
+}];
 
 
 app.get("/issues", function (req, res) {
-    var file = './storage/issues.txt'
-    jsonfile.readFile(file, function (err, obj) {
-        issues = obj
-        console.dir(obj);
-        console.log(obj);
-        res.send(issues);
-    })
+    let file = './storage/issues.txt'
+    issues = convertCSV(file)
+    res.send(issues);
 });
 
 app.get("/revenue", function (req, res) {
