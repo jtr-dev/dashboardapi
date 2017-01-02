18192eec6229ef363cd97966d52f5e7ab2de1a85
Tyler Roberts <dnbxna@gmail.com>
2017-01-02 13:25:50 -0500

    fix

--- server.js
+++ server.js
@@ -9,37 +9,30 @@ var newIssues = [{
 		repo: 'Career Builder',
 		issue: 'Can\'t login',
 		comment: ' Attack dog, run away and pretend to be victim with tail in the air',
-		opened: this.opened()
 	}, {
 		repo: 'Grocery App',
 		issue: 'Cafe c\'est non noir',
 		comment: 'J\'aime le cafè noir, avec la creme et du sugar. Mais, c\'est non noir a plus. ',
-		opened: this.opened()
 	}, {
 		repo: 'Grocery App ',
 		issue: 'Ou le pommes ',
 		comment: 'Je ne peux pas trouver les pommes, J\'ai regardé partout ',
-		opened: this.opened()
 	}, {
 		repo: 'Leo interdum lectus elementum',
 		issue: 'Lorem ipsum dolor sit amet.',
 		comment: '  Ignore the squirrels, you\'ll never catch them ',
-		opened: this.opened()
 	}, {
 		repo: 'Eu recusandae ',
 		issue: 'Lorem ipsum dolor sit amet.',
 		comment: ' Element convallis risus mauris nam magna wisi, hendrerit dui mauris tincidunt integer in a.',
-		opened: 'sun Dec 25 2016, 12:20'
 	}, {
 		repo: 'Nec porta ',
 		issue: 'Lorem ipsum dolor sit amet.',
 		comment: 'A claw then lick paws clean wash down prawns with a lap of carnation milk then retire to ',
-		opened: this.opened()
 	}, {
 		repo: 'lacus quam nunc eget',
 		issue: 'Lorem ipsum dolor sit amet.',
 		comment: 'Lay on arms while you\'re using the keyboard eat prawns daintily',
-		opened: this.opened()
 	}];
 
 
